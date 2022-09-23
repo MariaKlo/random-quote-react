21fdd77db3b04a4a35ccc769d2ab4dd2c3f36fdf
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-09-23 16:02:37 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import COLORS_ARRAY from './colorsArray';
+import './App.scss';
 
+let quoteDBURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+
+// 58:59
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [quote, setQuote] = useState("Life is 10% what happens to me and 90% of how I react to it.");
+    const [author, setAuthor] = useState("Charles Swindoll");
+    const [randomNumber, setRandomNumber] = useState(0);
+    const [quotesArray, setQuotesArray] = useState(null);
+    const [accentColor, setAccentColor] = useState('#282c34');
+
+    const fetchQuotes = async (url) => {
+        const response = await fetch(url);
+        const parsedJson = await response.json();
+        setQuotesArray(parsedJson.quotes);
+    }
+
+    useEffect(() => {
+        fetchQuotes(quoteDBURL);
+    }, [quoteDBURL]);
+
+    const getRandomQuote = () => {
+        let randomInteger = Math.floor(quotesArray.length * Math.random());
+        setRandomNumber(randomInteger);
+        setAccentColor(COLORS_ARRAY[randomInteger]);
+        setQuote(quotesArray[randomInteger].quote)
+        setAuthor(quotesArray[randomInteger].author)
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header" style={{backgroundColor: accentColor}}>
+                <div id="quote-box" style={{color: accentColor}}>
+                    <h1>Random Quote</h1>
+                    <p id="text"><FontAwesomeIcon icon={faQuoteLeft}/> {quote} <FontAwesomeIcon icon={faQuoteRight}/></p>
+                    <p id="author">- {author}</p>
+                    <div className="button">
+                        <a 
+                        href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)} 
+                        id="tweet-quote" 
+                        style={{backgroundColor: accentColor}}>
+                        <FontAwesomeIcon icon={faTwitter}/>
+                        </a>
+                        <button 
+                        id="new-quote" 
+                        onClick={() => getRandomQuote()}
+                        style={{backgroundColor: accentColor}}>Generate a Random Quote</button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
